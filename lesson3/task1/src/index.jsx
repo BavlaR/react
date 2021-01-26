ce15fb85a0e010cb57f370f86b4361d62d7bfc90
BavlaR <vdmmailbox1@gmail.com>
2021-01-26 15:09:07 +0200

    task1

--- lesson3/task1/src/index.jsx
+++ lesson3/task1/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.scss';
 import Search from './Search.jsx';
 
 ReactDOM.render(<Search name="Tom" />, document.querySelector('#root'));
