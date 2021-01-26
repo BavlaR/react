390aa84150da774e052159c43952036a6793ad8f
BavlaR <vdmmailbox1@gmail.com>
2021-01-26 14:42:01 +0200

    task1

---
+++ lesson3/task1/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchField from './Search.jsx';
+
+ReactDOM.render(<SearchField name="Tom" />, document.querySelector('#root'));
