8e3bee054b2d0b3e92e8ac485a3053b4ab6b1318
BavlaR <vdmmailbox1@gmail.com>
2021-01-27 15:31:35 +0200

    task3

--- lesson3/task3/src/App.jsx
+++ lesson3/task3/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Comment from './Comment';
 
-const userInfo = {
+const user = {
    name: 'Tom',
    avatarUrl: 'https://avatars1.githubusercontent.com',
 };
 
-export default () => <Comment author={userInfo} text="Good job!" date={new Date()} />;
+export default () => <Comment author={user} text="Good job!" date={new Date()} />;
--- lesson3/task3/src/UserInfo.jsx
+++ lesson3/task3/src/UserInfo.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './userInfo.scss';
 import Avatar from './Avatar';
 
-export default props => (
+const UserInfo = props => (
    <div className="user-info">
       <Avatar avatarUrl={props.user.avatarUrl} name={props.user.name} />
       <div className="user-info__name">{props.user.name}</div>
    </div>
 );
+
+export default UserInfo;
