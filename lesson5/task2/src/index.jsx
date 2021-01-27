2b0f8b1a9b1c7f843c4b2eda94ca07f03e4025fa
BavlaR <vdmmailbox1@gmail.com>
2021-01-27 23:54:09 +0200

    task3

--- lesson5/task2/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import GoodButton from './GoodButton';
-
-ReactDOM.render(<GoodButton />, document.querySelector('#root'));
