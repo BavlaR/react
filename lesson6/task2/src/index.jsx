b356ec8c9ceb642f406e2b6f32c27e8d4e3f20cf
BavlaR <vdmmailbox1@gmail.com>
2021-01-30 15:38:08 +0200

    task2

---
+++ lesson6/task2/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Mailbox from './Mailbox';
+
+ReactDOM.render(<Mailbox unreadMessages={[]} />, document.querySelector('#root'));
