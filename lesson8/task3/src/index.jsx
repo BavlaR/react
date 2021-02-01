b50f7c097b31eadf2a761d96e494cce8ec340820
BavlaR <vdmmailbox1@gmail.com>
2021-02-01 17:58:27 +0200

    task3

--- lesson8/task3/src/index.jsx
+++ lesson8/task3/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Dimension from './Dimensions';
+import Dimension from './Dimension';
 
 ReactDOM.render(<Dimension />, document.querySelector('#root'));
