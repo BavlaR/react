5a3dcbe3a68366f4f4ee8c4cdba7544711bbe96d
BavlaR <vdmmailbox1@gmail.com>
2021-02-15 12:54:44 +0200

    hooks

--- live/less13addHW/src/User.jsx
+++ live/less13addHW/src/User.jsx
@@ -1,65 +1,48 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-class User extends Component {
-   state = {
+const User = p => {
+   // input init state
+   // output array (state val + func)
+   const [userData, setUserData] = useState({
       avatar: null,
       name: null,
       location: null,
-   };
+   });
 
-   componentDidMount() {
-      this.getUserData(this.props.match.params.userId);
-   }
+   const { userId } = useParams(); // Hook rout
 
-   componentDidUpdate(prevProps) {
-      const currUser = this.props.match.params.userId;
-      if (prevProps.match.params.userId !== currUser) {
-         this.getUserData(currUser);
-      }
-   }
+   // input: callback, [arr]
+   // output: undef
 
-   getUserData = userId => {
+   //callback input: -
+   // callback output: undef/fn
+   useEffect(() => {
       fetch(`https://api.github.com/users/${userId}`)
          .then(res => res.json())
          .then(userData => {
             const { avatar_url, name, location } = userData;
-            this.setState({
+            setUserData({
                avatar: avatar_url,
                name: name,
                location: location,
             });
          });
-   };
+   }, [userId]);
 
-   render() {
-      const { avatar, name, location } = this.state;
+   const { avatar, name, location } = userData;
 
-      return (
-         <div className="user">
-            <img alt="User Avatar" src={avatar} className="user__avatar" />
-            <div className="user__info">
-               <span className="user__name">{name}</span>
-               <span className="user__location">{location}</span>
-            </div>
+   if (!avatar || !name || !location) return null;
+
+   return (
+      <div className="user">
+         <img alt="User Avatar" src={avatar} className="user__avatar" />
+         <div className="user__info">
+            <span className="user__name">{name}</span>
+            <span className="user__location">{location}</span>
          </div>
-      );
-   }
-}
+      </div>
+   );
+};
 
 export default User;
-
-// const [userData, setUserData] = useState(null);
-
-// useEffect(() => {
-//    fetch(`https://api.github.com/users/${match.params.userId}`)
-//       .then(res => {
-//          if (res.ok) return res.json();
-
-//          throw new Error();
-//       })
-//       .then(userData => setUserData(userData));
-// }, [match.params.userId]);
-
-// if (!userData) return null;
-
-// const { name, location, avatar_url } = userData;
